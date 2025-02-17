93791f441343ed5da7a40bbd9a8b7d153bb42935
chukwubuikem <modilimhilary@gmail.com>
2025-02-17 21:57:37 +0100

    update

---
+++ practice/TestProj/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Program
+{
+    public class Value
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Welcome to the Note Creation Page");
+            System.Console.WriteLine("Select an option to continue");
+            System.Console.WriteLine("1. Enter the Title of the Note");
+            System.Console.WriteLine("2. Enter the Content of the Note");
+            System.Console.WriteLine("");
+        }
+        void CreateNote()
+        {
+
+        }
+    }
+}
